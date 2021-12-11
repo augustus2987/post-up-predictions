a4c22b6e60bc2b3d2886ca3d1a061544b9dc7cbb
Miles <miles@starfishspace.com>
2021-12-11 14:56:29 -0700

    Whatever I added yesterday

--- apiTrial1.py
+++ apiTrial1.py
@@ -32,7 +32,47 @@ class NBA_Data:
 
         self.features_names = ['away_defensive_rebound_percentage', 'away_effective_field_goal_percentage', 'away_offensive_rebound_percentage', 'away_offensive_rating',
         'home_defensive_rebound_percentage', 'home_effective_field_goal_percentage', 'home_offensive_rebound_percentage', 'home_offensive_rating']
-        self.home_features 
+        self.home_features  = ['home_defensive_rebound_percentage']
+
+        self.features_names = [
+            'assist_percentage',
+            'assists',	
+            'block_percentage',	
+            'blocks',
+            'defensive_rating',	
+            'defensive_rebound_percentage',	
+            'defensive_rebounds',	
+            'effective_field_goal_percentage',	
+            'field_goal_attempts',	
+            'field_goal_percentage',	
+            'field_goals',	
+            'free_throw_attempt_rate',	
+            'free_throw_attempts',	
+            'free_throw_percentage',	
+            'free_throws',	
+            'minutes_played',	
+            'offensive_rating',	
+            'offensive_rebound_percentage',	
+            'offensive_rebounds',
+            'personal_fouls',	
+            'points',	
+            'steal_percentage',	
+            'steals',	
+            'three_point_attempt_rate',	
+            'three_point_field_goal_attempts',	
+            'three_point_field_goal_percentage',	
+            'three_point_field_goals',	
+            'total_rebound_percentage',	
+            'total_rebounds',	
+            'true_shooting_percentage',	
+            'turnover_percentage',	
+            'turnovers',	
+            'two_point_field_goal_attempts',	
+            'two_point_field_goal_percentage',	
+            'two_point_field_goals',	
+        ]
+        self.home_feature_names = ['home_' + stat for stat in self.features_names]
+        self.away_feature_names = ['away_' + stat for stat in self.features_names]
 
 
     # Set the schedule for the teams.
@@ -84,14 +124,55 @@ class NBA_Data:
     # Take a list of boxscore indices of games and output the stats from each game. 
     # Input1: list of strings of feature names 
     def get_game_data(self, features_names=None):
-    
         for boxscore in self.team1_schedule:
             gamedata = Boxscore(str(boxscore)).dataframe
-            self.team1_data.append(gamedata.loc[:,self.features_names].values) # turn into nparray
+            self.team1_data.append(gamedata)
+            # self.team1_data.append(gamedata.loc[:,self.features_names].values) # turn into nparray
 
         for boxscore in self.team2_schedule:
             gamedata = Boxscore(str(boxscore)).dataframe
-            self.team2_data.append(gamedata.loc[:,self.features_names].values) # turn into nparray
+            self.team2_data.append(gamedata)
+            # self.team2_data.append(gamedata.loc[:,self.features_names].values) # turn into nparray
+
+
+    # Splits data from all games into home data and away data (two seperate lists)
+    def split_games(self, team_data, team_name):
+
+        home_games = []
+        away_games = []
+
+        for game in team_data:
+
+            if game['winning_abbr'] == team_name:
+                if game['winner'] == 'Home':
+                    home_games.append(game)
+                else:
+                    away_games.append(game)
+            else: #game['losing_abbr'] == team_name
+                if game['winner'] == 'Home':
+                    away_games.append(game)
+                else:
+                    home_games.append(game)
+
+        return home_games, away_games
+
+
+    # formats all data
+    #def get_team_data(self, location, games):
+
+    def get_final_data(self):
+
+        self.get_team_schedules()
+        self.get_game_data()
+
+        team1_home_data, team2_away_data = self.split_games(self.team1_data, self.team1)
+        team2_home_data, team2_away_data = self.split_games(self.team2_data, self.team2)
+
+        #team1_home_data = team1_home_data[stat for stat in self.home_features_names]
+
+
+
+        
 
 s = Schedule('DEN', '2019')
 bs = []
